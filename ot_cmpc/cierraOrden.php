e3a46adff49668d5cf0ffda6e39d9ba0ca325e7b
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 16:05:10 -0300

    Update cierraOrden.php

--- ot_cmpc/cierraOrden.php
+++ ot_cmpc/cierraOrden.php
@@ -1,14 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
-
-
 $output = json_encode(['status' => 'recieved']);
-header('Content-Length: '.strlen($output));
-header('Content-Type: application/json');
 echo $output;
 exit;
 /* 	include "conectar.php";
