0e81e93a7b7b6bcc44d443f064c08dd0c34525bf
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 15:56:21 -0300

    Update cierraOrden.php

--- ot_cmpc/cierraOrden.php
+++ ot_cmpc/cierraOrden.php
@@ -4,9 +4,7 @@ header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Conte
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-    die();
-}
+
 
 $output = json_encode(['status' => 'recieved']);
 header('Content-Length: '.strlen($output));
