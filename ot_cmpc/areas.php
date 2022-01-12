89d3225846a28ba73af2a88f622e9172d1152c79
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 00:47:08 -0300

    Update areas.php

--- ot_cmpc/areas.php
+++ ot_cmpc/areas.php
@@ -10,10 +10,12 @@
 		printf("Errormessage: %s\n", mysqli_error($conn));
 	$rows = array();
 	while($r = mysqli_fetch_array($result)) {
-		$rows[] = $r;		
+		$rows[] = $r;
+		
 	}
-	if (empty($rows))
-		echo "is empty";	
+	
+	foreach ( $conn->query($sql) as $fila ) 
+	echo json_encode($fila);
 	echo json_encode($rows);
 	$conn->close();
 
