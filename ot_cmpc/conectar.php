812042da2cd3cf6aac19651934f350a58f3ae743
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-11 21:50:43 -0300

    Update conectar.php

--- ot_cmpc/conectar.php
+++ ot_cmpc/conectar.php
@@ -4,29 +4,16 @@ header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Conte
 header("Content-Type: text/html; charset=utf-8");
 require_once('connect.php');
 $method = $_SERVER['REQUEST_METHOD'];
-echo "algo";
 function conectarDB(){
-
-  $servidor = "127.0.0.1:1234";
-  $usuario = "root";
-  $password = "031203";
-  $bd = "ot_cmpc";
-  //$bd = "cajaherr_datos";
-  
-
- 	$conexion = mysqli_connect($servidor, $usuario, $password,$bd);
-
-        if($conexion){
-            echo "conectado";
-        }else{
-            echo 'Ha sucedido un error inexperado en la conexion de la base de datos';
-        }
-
-      
-	
-/* 	$myConecction = new MySQLDB($servidor, $usuario, $password,$bd);
-	$conexion = $myConecction->connect();
-	 */
+	$servidor = "localhost";
+	$usuario = "root";
+	$password = "031203";
+	$bd = "ot_cmpc";
+	$conexion = mysqli_connect($servidor, $usuario, $password,$bd);
+	if($conexion)
+		echo "conectado";
+	else
+		echo 'Ha sucedido un error inexperado en la conexion de la base de datos';
 	return $conexion;
 }
 ?>
