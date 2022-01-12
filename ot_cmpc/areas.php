b0d573320931ef98bc4fca727a1312b2d23cca99
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 09:29:30 -0300

    Update areas.php

--- ot_cmpc/areas.php
+++ ot_cmpc/areas.php
@@ -1,5 +1,8 @@
 <?php
 	include "conectar.php";
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
     $conn = conectarDB();
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
