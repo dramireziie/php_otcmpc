48fa9c95da2229d3d39c1504eb7422226bd0d4fd
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 09:23:48 -0300

    Update areas.php

--- ot_cmpc/areas.php
+++ ot_cmpc/areas.php
@@ -5,7 +5,31 @@
 	  die("Connection failed: " . $conn->connect_error);
 	}
 	$sql = "Select * from area";
+	try {
+            // Try Connect to the DB with new MySqli object - Params {hostname, userid, password, dbname}
+            //$mysqli = new mysqli("localhost", "root", "", "mysqli_examples");
+
+            
+            $statement = $conn->prepare($sql);
+
+
+            $statement->execute(); // Execute the statement.
+            $result = $statement->get_result(); // Binds the last executed statement as a result.
+
+            echo json_encode(($result->fetch_assoc())); // Parse to JSON and print.
+  
+        } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
+            echo "MySQLi Error Code: " . $e->getCode() . "<br />";
+            echo "Exception Msg: " . $e->getMessage();
+            exit(); // exit and close connection.
+        }
+
+    $mysqli->close(); // finally, close the connection
 	
+	
+	
+	
+	/* 
 	$result = $conn->query($sql);
 	$rows = array();
 	$i=0;
@@ -25,7 +49,7 @@
 	 */
 	//foreach ( $conn->query($sql) as $fila ) 
 	//echo json_encode($fila);
-	echo json_encode($rows[0]);
-	$conn->close();
+/* 	echo json_encode($rows[1]);
+	$conn->close(); */ */
 
 ?>
