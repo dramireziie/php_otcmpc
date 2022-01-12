91061594bf43cc893fa64f3927d134c7ced636d4
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 12:01:13 -0300

    Update areas.php

--- ot_cmpc/areas.php
+++ ot_cmpc/areas.php
@@ -1,55 +1,19 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
-	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-	header("Content-Type: text/html; charset=utf-8");
 	include "conectar.php";
     $conn = conectarDB();
 	if ($conn->connect_error) {
 	  die("Connection failed: " . $conn->connect_error);
 	}
 	$sql = "Select * from area";
+	$result = mysqli_query($conn,$sql);
 	
-	foreach ( $conn->query($sql) as $record) {
-		$arr[] = array(
-					'idArea' => $record['idArea'],
-					'nombreArea' => $record['nombreArea']
+	$rows = array();
+	while($r = mysqli_fetch_array($result)) {
+		$rows[] = $r;
+	}
+	//foreach ( $conn->query($sql) as $fila ) 
 
-				); 
-		//assign each sub-array to the newly created array
-		$arr2[] = array('conectado'=>$record['nombreArea'], 'error' => 'La clave es incorrecta, vuelva a intentarlo.');
-	} 
-	
-	echo json_encode($arr2);
-	
-	try {
-/*             $statement = $conn->prepare($sql);
-
-
-            $statement->execute(); // Execute the statement.
-            $result = $statement->get_result(); // Binds the last executed statement as a result. */
-/* 			if ($result->num_rows >= 1 && !is_null($result->num_rows)) {
-				$datos [] = $result->fetch_array();  
-			}
-				
-			else 
-				echo "no hay resultados";
-			 */
-			$result = $conn->query($sql); 
-			while ($row = $result->fetch_assoc()) {
-					$statsArray[] = $row;
-			}
-			echo json_encode($statsArray);
-            printf("nada2",json_encode(($fila))); // Parse to JSON and print.
-			
-        } catch (mysqli_sql_exception $e) { // Failed to connect? Lets see the exception details..
-            echo "MySQLi Error Code: " . $e->getCode() . "<br />";
-            echo "Exception Msg: " . $e->getMessage();
-            exit(); // exit and close connection.
-        }
-
-    $conn->close(); // finally, close the connection
-	
-	
-	
+	echo json_encode($rows);
+	$conn->close();
 
 ?>
