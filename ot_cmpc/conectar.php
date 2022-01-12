dbe8f48cb2a27a92cee6200db0fa809cad969c2a
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 09:27:41 -0300

    Update conectar.php

--- ot_cmpc/conectar.php
+++ ot_cmpc/conectar.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Content-Type: text/html; charset=utf-8");
