aabe1a29ac82bf76e6e4b19d0de7f9fe9b4b0f52
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 15:55:08 -0300

    Update cierraOrden.php

--- ot_cmpc/cierraOrden.php
+++ ot_cmpc/cierraOrden.php
@@ -1,5 +1,4 @@
 <?php
-// Allow from any origin
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
