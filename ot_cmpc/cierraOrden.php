a84096be91cd45da4eb23b79abaed1d5ef7c06ea
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 20:49:46 -0300

    submit

--- ot_cmpc/cierraOrden.php
+++ ot_cmpc/cierraOrden.php
@@ -1,6 +1,7 @@
 <?php
-include "core.php";
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Content-Type: text/html; charset=utf-8");
 $method = $_SERVER['REQUEST_METHOD'];
 $output = json_encode(['status' => 'recieved']);
 echo $output;
--- ot_cmpc/ordenes.php
+++ ot_cmpc/ordenes.php
@@ -1,4 +1,8 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Content-Type: text/html; charset=utf-8");
+	$method = $_SERVER['REQUEST_METHOD'];
 	include "conectar.php";
     $conn = conectarDB();
 	if ($conn->connect_error) {
