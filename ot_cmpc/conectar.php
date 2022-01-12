09aa17a40ab8ae54d63d0e97c18652fbc9cf3b70
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-12 11:59:08 -0300

    Update conectar.php

--- ot_cmpc/conectar.php
+++ ot_cmpc/conectar.php
@@ -17,7 +17,7 @@ function conectarDB(){
 	$conexion = mysqli_connect($servidor, $usuario, $password,$bd);
 	
 	if($conexion)
-		mysql_set_charset("UTF8", $conexion);
+		$conexion->set_charset("utf8");
 	else
 		echo 'Ha sucedido un error inexperado en la conexion de la base de datos';
 	return $conexion;
