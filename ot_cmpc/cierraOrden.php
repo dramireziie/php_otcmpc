79a360ae9cf2c656e9ac949d9e1ebd059fc28a4d
dramireziie <81206697+dramireziie@users.noreply.github.com>
2022-01-13 22:00:17 -0300

    Update cierraOrden.php

--- ot_cmpc/cierraOrden.php
+++ ot_cmpc/cierraOrden.php
@@ -1,6 +1,5 @@
 <?php
-echo "hi";
-
+echo json_encode(array('actualizado'=>true, 'msg' => 'Orden cerrada con éxito!'));
 
 /* header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
